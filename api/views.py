65e63d22cd3b71838e3e1ccd6bd0f12d434fc6c7
olatomide007 <olatomideafolabi@gmail.com>
2022-10-29 19:23:00 +0100

    Modify for returning json result instead of api response kind

--- api/views.py
+++ api/views.py
@@ -1,12 +1,14 @@
-from rest_framework.response import Response
+# from rest_framework.response import Response. To return a api response
 from rest_framework.decorators import api_view
+from django.http import JsonResponse # To return json
 
 @api_view(['GET'])
 def student (request):
     person = {
         'slackUsername': 'Olatomide',
-        'backend' : 'True',
-        'age' : '30',
+        'backend': True,
+        'age' : 30,
         'Bio' : 'I am cool person that loves to meet people. My aim is to learn better'
     }
-    return Response(person)
+    #return Response(person)
+    return JsonResponse(person)
